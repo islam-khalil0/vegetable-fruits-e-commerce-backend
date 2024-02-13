65fda61e0623d72aaf8999dc2794f2ca9a1a0c25
islam <eslamkhalil851@gmail.com>
2024-02-13 22:18:49 +0200

    add access to localhost for development

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ const Offers = require("./models/Offers");
 
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 mongoose
   .connect(
