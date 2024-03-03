d577346cb54077433ec5595a11163ecf292321d8
islam <eslamkhalil851@gmail.com>
2024-03-04 01:17:58 +0200

    create location of images

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, "./images");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
