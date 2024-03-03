218b7a4ac8e2bf227328cafcfd16c2b45160b024
islam <eslamkhalil851@gmail.com>
2024-03-04 01:31:23 +0200

    add public folder

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./images");
+    cb(null, "./public");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
